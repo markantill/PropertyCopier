24dd7bd949e57200033f70b42afb68a922f0e4ee
Mark Antill <mark.antill@gmail.com>
2017-03-27 17:06:49 +0100

    Create multi-targeted build with strong name

--- build.bat
+++ build.bat
@@ -1,8 +1,4 @@
 @echo on
-set config=%1
-if "%config%" == "" (
-   set config=Release
-)
 
 set version=
 if not "%PackageVersion%" == "" (
@@ -17,19 +13,29 @@ REM Package restore
 call %NuGet% restore PropertyCopier\packages.config -OutputDirectory %cd%\packages -NonInteractive
 
 REM Build
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild PropertyCopier.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild PropertyCopier.sln /p:Configuration="Release 4.0" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild PropertyCopier.sln /p:Configuration="Release 4.5" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
 
 REM Unit tests
-packages\NUnit.Runners.2.6.4\tools\nunit-console.exe PropertyCopier.Tests\bin\%config%\PropertyCopier.Tests.dll
+packages\NUnit.Runners.2.6.4\tools\nunit-console.exe "PropertyCopier.Tests\bin\Release 4.0\PropertyCopier.Tests.dll"
+if not "%errorlevel%"=="0" goto failure
+packages\NUnit.Runners.2.6.4\tools\nunit-console.exe "PropertyCopier.Tests\bin\Release 4.5\PropertyCopier.Tests.dll"
 if not "%errorlevel%"=="0" goto failure
 
 REM Package
-mkdir Build
-mkdir Build\net40
-copy PropertyCopier\bin\%config%\PropertyCopier.dll Build\net40
-copy PropertyCopier\bin\%config%\PropertyCopier.pdb Build\net40
+mkdir output\lib
+mkdir output\lib\net40
+mkdir output\lib\net45
 
-call %NuGet% pack "PropertyCopier\PropertyCopier.csproj" -IncludeReferencedProjects -symbols -o Build\net40 -p Configuration=%config% %version%
+copy "PropertyCopier\bin\Release 4.0\PropertyCopier.dll" "output\lib\net40"
+copy "PropertyCopier\bin\Release 4.0\PropertyCopier.pdb" "output\lib\net40"
+
+copy "PropertyCopier\bin\Release 4.5\PropertyCopier.dll" "output\lib\net45"
+copy "PropertyCopier\bin\Release 4.5\PropertyCopier.pdb" "output\lib\net45"
+
+cd output
+call ..\%NuGet% pack PropertyCopier.nuspec -IncludeReferencedProjects -symbols -o ..\Build
+cd ..
 
 :success
 echo YAY
